33f8f7a80fc8ade105ed9f5d11c93fb4e85d0e44
David Jonas <davidjonasdesign@gmail.com>
2016-01-29 16:12:44 +0100

    Added code to build cpp-ethereum from source on raspberry pi

--- installers/rasppi.sh
+++ installers/rasppi.sh
@@ -3,6 +3,7 @@
 echo "========== Installing Ethereum Blockchain and V2_Blockchain configuration =========="
 cd $HOME
 echo "Getting V2_Blockchain tools"
+sudo apt-get update
 sudo apt-get -y install git
 git clone https://github.com/davidjonas/v2_blockchain.git blockchain
 cd blockchain
@@ -13,8 +14,18 @@ rm geth-ARM-latest.tar.bz2
 echo "Adding to the PATH variable..."
 export PATH=$PATH:$HOME/blockchain
 echo "export PATH=\$PATH:\$HOME/blockchain" >> ~/.bashrc
+source ~/.bashrc
+echo "Building Solidity compiler"
+git clone https://github.com/ethereum/cpp-ethereum.git
+mkdir cpp-ethereum/build
+cd cpp-ethereum/build
+cmake -DJSONRPC=OFF -DMINER=OFF -DETHKEY=OFF -DSERPENT=OFF -DGUI=OFF -DTESTS=OFF -DJSCONSOLE=OFF ..
+make -j4
+make install
+which solc
+cd $HOME
 echo "installing Nodejs tools for GPIO and Ethereum..."
-sudo apt-get -y install solc nodejs npm
+sudo apt-get -y install nodejs npm
 mkdir nodejs
 cd nodejs
 npm install web3 onoff gpio-stream
